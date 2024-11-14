401174abbed58517897efe552991b1ce80184d8e
Natanael Alves Gabriel <natasnael2002@gmail.com>
2024-11-13 21:08:45 -0300

    change open ai to cohere ai

--- main.py
+++ main.py
@@ -4,38 +4,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# GPT_KEY_LEO
-# GPT_KEY_NATAS
-client = openai(api_key = os.getenv("GPT_KEY_NATAS")) 
+
 pc_api = api(region="us")
 
-specialization_prompt = "You are a specialized assistant for computer specifications."
+import cohere
+
+client = cohere.Client(os.getenv("COHERE_KEY_NATAS"))
 
 def generate_response(prompt):
-  response = client.chat.completions.create(
-    messages=[
-      {
-        "role": "system",
-        "content":
-          {
-            "type": "text",
-            "text": specialization_prompt
-          }
-      },
-      {
-        "role": "user",
-        "content":
-          {
-            "type": "text",
-            "text": prompt
-          }
-      }
-    ],
-    model="gpt-3.5-turbo",
-    max_tokens=150,
-    temperature=0.5
-  )
-  return response['choices'][0]['message']['content'].strip()
+    response = client.generate(
+        model="command-xlarge",
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.5
+    )
+    return response.generations[0].text.strip()
 
 def get_part_info(part_type):
   try:
